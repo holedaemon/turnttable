07ed48553e1e60aed18e8bc7ce4a19d44e2caa58
Max Reiter <max.m.reiter@gmail.com>
2023-02-04 17:49:34 -0700

    edit and delete pages for admin

--- internal/web/admin.go
+++ internal/web/admin.go
@@ -1,7 +1,9 @@
 package web
 
 import (
+	"database/sql"
 	"encoding/csv"
+	"errors"
 	"net/http"
 	"strings"
 	"time"
@@ -24,6 +26,12 @@ func (s *Server) routeAdmin(r chi.Router) {
 
 	r.Get("/insert/bulk", s.adminBulkInsert)
 	r.Post("/insert/bulk", s.postAdminBulkInsert)
+
+	r.Get("/edit/{id}", s.adminEdit)
+	r.Post("/edit/{id}", s.postAdminEdit)
+
+	r.Get("/delete/{id}", s.adminDelete)
+	r.Post("/delete/{id}", s.postAdminDelete)
 }
 
 func (s *Server) adminAuth(next http.Handler) http.Handler {
@@ -187,4 +195,135 @@ func (s *Server) postAdminBulkInsert(w http.ResponseWriter, r *http.Request) {
 	}
 
 	s.statusPage(w, "yay dood", "Wow cool okay", "Bulk insert has completed")
+}
+
+func (s *Server) adminEdit(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	ctx := r.Context()
+
+	row, err := models.Records(qm.Where("id = ?", id)).One(ctx, s.DB)
+	if err != nil {
+		if errors.Is(sql.ErrNoRows, err) {
+			s.badRequest(w, "No record by that \"id\" exists")
+			return
+		}
+
+		ctxlog.Error(ctx, "error getting record", zap.Error(err), zap.String("id", id))
+		s.internalError(w)
+		return
+	}
+
+	templates.WritePageTemplate(w, &templates.AdminEditPage{
+		Record: row,
+	})
+}
+
+func (s *Server) postAdminEdit(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	ctx := r.Context()
+
+	if err := r.ParseForm(); err != nil {
+		ctxlog.Error(ctx, "error parsing form", zap.Error(err))
+		s.internalError(w)
+		return
+	}
+
+	title := r.FormValue("title")
+	medium := mediumMap[strings.ToLower(r.FormValue("medium"))]
+
+	exists, err := models.Records(
+		qm.Where("title ILIKE ? AND medium = ? AND id != ?", title, medium, id),
+	).Exists(ctx, s.DB)
+	if err != nil {
+		ctxlog.Error(ctx, "error querying for record", zap.Error(err))
+		s.internalError(w)
+		return
+	}
+
+	if exists {
+		s.statusPage(w, "lol", "Your title & medium conflict with another record!!", "Silly goose")
+		return
+	}
+
+	released := r.FormValue("release")
+	pur := r.FormValue("purchased")
+
+	rt, err := time.Parse("2006-01-02", released)
+	if err != nil {
+		ctxlog.Error(ctx, "error parsing release time", zap.Error(err))
+		s.badRequest(w, "Release time formatting is bad")
+		return
+	}
+
+	pt, err := time.Parse("2006-01-02", pur)
+	if err != nil {
+		ctxlog.Error(ctx, "error parsing purchase time", zap.Error(err))
+		s.badRequest(w, "Purchased time formatting is bad")
+		return
+	}
+
+	record := models.Record{
+		Title:     r.FormValue("title"),
+		Artist:    r.FormValue("artist"),
+		Label:     r.FormValue("label"),
+		CN:        r.FormValue("cn"),
+		Genre:     r.FormValue("genre"),
+		Released:  rt,
+		Purchased: null.TimeFrom(pt),
+		Medium:    medium,
+	}
+
+	if err := record.Insert(ctx, s.DB, boil.Infer()); err != nil {
+		ctxlog.Error(ctx, "error inserting", zap.Error(err))
+		s.internalError(w)
+		return
+	}
+
+	s.statusPage(w, "Updated Record", "wow okay cool", "Record has been updated")
+}
+
+func (s *Server) adminDelete(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	ctx := r.Context()
+
+	row, err := models.Records(qm.Where("id = ?", id)).One(ctx, s.DB)
+	if err != nil {
+		if errors.Is(sql.ErrNoRows, err) {
+			s.badRequest(w, "No record by that \"id\" exists")
+			return
+		}
+
+		ctxlog.Error(ctx, "error getting record", zap.Error(err), zap.String("id", id))
+		s.internalError(w)
+		return
+	}
+
+	templates.WritePageTemplate(w, &templates.AdminDeletePage{
+		Record: row,
+	})
+}
+
+func (s *Server) postAdminDelete(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	ctx := r.Context()
+
+	row, err := models.Records(qm.Where("id = ?", id)).One(ctx, s.DB)
+	if err != nil {
+		if errors.Is(sql.ErrNoRows, err) {
+			s.badRequest(w, "No record by that \"id\" exists")
+			return
+		}
+
+		ctxlog.Error(ctx, "error getting record", zap.Error(err), zap.String("id", id))
+		s.internalError(w)
+		return
+	}
+
+	if err := row.Delete(ctx, s.DB); err != nil {
+		ctxlog.Error(ctx, "error deleting row", zap.Error(err))
+		s.internalError(w)
+		return
+	}
+
+	s.statusPage(w, ":3", "bye sis", "Record has been deleted")
 }
